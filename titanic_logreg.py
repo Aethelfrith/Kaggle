52b7c0ebaa7311684291ae54380843ed5eb74138
Aethelfrith <52699977+Aethelfrith@users.noreply.github.com>
2019-07-24 14:08:02 +0200

    Add training curve functionality

--- titanic_logreg.py
+++ titanic_logreg.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report as cr
 from sklearn import preprocessing as pp
+from sklearn.model_selection import learning_curve
 
 
 #Define functions
@@ -102,7 +103,88 @@ def polynomialize_df(df, poly_degree, include_bias, interaction_only = False):
 	#Make a new dataframe
 	df = pd.DataFrame(np_array_poly, columns = [polyfeature_names,])
 	return df
-	
+
+def plot_learning_curve(estimator, X, y, title = None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
 	
 
 #END Define functions
@@ -134,7 +216,7 @@ X_train_val,y_train_val = preprocess_titanic_data(training_data, features = trai
 X_test = preprocess_titanic_data(test_data,features = train_features,exists_y = False)
 
 #Add polynomial features
-poly_degree = 1
+poly_degree = 2
 include_bias = False
 interaction_only = False
 X_train_val = polynomialize_df(X_train_val, poly_degree, include_bias, interaction_only)
@@ -173,8 +255,8 @@ X_train_val = normalise_entire_dataframe(X_train_val, mode = normalise_mode)
 
 #TRAINING
 #Set global random seed
-#random_seed = 1234
-random_seed = 2017
+random_seed = 1234
+#random_seed = 2017
 #random_seed = 3972153975
 
 #Subset training and validation data
@@ -189,7 +271,8 @@ is_fit_intercept = True
 class_weight_mode = None
 regularisation_style = 'l2'
 #regularisation_style = 'l1'
-max_iter = 200
+#max_iter = 200
+max_iter = 300
 estimator = LogisticRegression(fit_intercept = is_fit_intercept, class_weight = class_weight_mode, C = reg_param_C, solver = lr_solver, multi_class = 'ovr',penalty = regularisation_style,random_state = random_seed, max_iter = max_iter)
 
 estimator.fit(X_train,np.ravel(y_train))
@@ -207,9 +290,36 @@ bias_term = estimator.intercept_
 print("Intercept: \n",bias_term)
 
 #Plot the importances in a bar plot
-importance_df.plot(kind='bar',figsize = (10,6))
-plt.legend(ncol=3)
+#importance_df.plot(kind='bar',figsize = (10,6))
+#plt.legend(ncol=3)
+#plt.show()
+
+
+#Plot a few dimensions of the data
+#plot_column_1 = 'Sex'
+#plot_column_2 = 'Pclass'
+#plot_column_3 = 'SibSp'
+#plot_classes = y_train
+#plt_alpha = 0.2
+#plt.scatter(x=X_train[plot_column_1].values,y=X_train[plot_column_2].values,alpha = plt_alpha, s=100*X_train[plot_column_3].values, c=plot_classes.values, cmap = 'viridis')
+#plt.xlabel(plot_column_1)
+#plt.ylabel(plot_column_2)
+#plt.show()
+
+
+#Generate data for a training curve
+#train_sizes = [0.5,0.7,0.9]
+training_curve_title = 'Logistic regression classifier'
+train_val_split_folds = 5
+train_sizes = np.linspace(0.04,1.0,20)
+#train_size, train_scores, valid_scores = learning_curve(estimator,X_train_val, np.ravel(y_train_val), train_sizes = train_sizes, cv = train_val_split_folds)
+
+plot_learning_curve(estimator, X_train_val, np.ravel(y_train_val), title = training_curve_title, cv=train_val_split_folds,train_sizes = train_sizes)
 plt.show()
+
+#print(train_size)
+#print(train_scores)
+
 
 #VALIDATION
 #Display the error metrics on the training data
